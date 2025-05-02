3655895ca7a18ec2a4b56e782f3ee9dc04aa3dc1
khanhnat2710 <khanhnat2710@gmail.com>
2025-05-02 10:38:53 +0700

    code login

---
+++ project/admin/login/login-process.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+    //lấy username và password từ form
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    //Mở kết nối
+    include_once "../connection/open.php";
+    //viết sql
+    $sql = "SELECT *, COUNT(ADMIN_ID) AS count_id FROM admins WHERE USERNAME='$username'";
+    //chạy sql
+    $result = mysqli_query($connection, $sql);
+    //đóng kết nối
+    include_once "../connection/close.php";
+    //kiểm tra username và password có đúng không
+    foreach ($result as $row){
+        if($row['count_id'] == 0){
+            //quay về trang login
+            header("Location: login.php");
+        } else {
+            //lưu account lên session
+            $_SESSION['ADMIN_ID'] = $row['ADMIN_ID'];
+            $_SESSION['username'] = $row['username'];
+            //quay về trang admin
+            header("location: ../../layouts/layout.php");
+        }
+    }
+?>
---
+++ project/admin/login/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <form method="post" action="login-process.php">
+        <label for="username">Username: </label><input type="text" name="username" id="username"><br>
+        <label for="password">Password: </label><input type="password" name="password" id="password"><br>
+        <button>Login</button>
+    </form>
+</body>
+</html>
