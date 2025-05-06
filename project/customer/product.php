85fba7c8aecae1f2dbb249c19a7c43fbeb2e45b6
khanhnat2710 <khanhnat2710@gmail.com>
2025-05-06 08:24:40 +0700

    sua code

--- project/customer/product.php
+++ project/customer/product.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Chi Tiết Sản Phẩm - Samsung Galaxy S24 Ultra</title>
+  <title>Chi Tiết Sản Phẩm</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
